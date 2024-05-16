f816cdcf980a06f4225a0f8b394db5ec2b68b19d
Diogotb <digogo@gmail.com>
2024-05-16 20:52:12 -0300

    16/05.v1

--- src/main/java/br/com/diogotb/loja_livros/Controller/LivrosController.java
+++ src/main/java/br/com/diogotb/loja_livros/Controller/LivrosController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
 import br.com.diogotb.loja_livros.Repository.LivrosRepository;
 import br.com.diogotb.loja_livros.Model.Livro;
@@ -22,14 +23,19 @@ public class LivrosController {
     @Autowired
     LivrosRepository livrosRepository;
 
-    @GetMapping("/livros")
-    public List<Livro> list() {
-        return (List<Livro>) this.livrosRepository.findAll();    
+    @GetMapping("/livros-list")
+    public ModelAndView list() {
+        ModelAndView mv = new ModelAndView("/livros-list");
+        mv.addObject("livros", livrosRepository.findAll());
+        return mv ;
     }
 
-    @PostMapping("/livros")
-    public Livro create(@RequestBody Livro livro) {
-        return this.livrosRepository.save(livro);
+    @PostMapping("/livros-add")
+    public ModelAndView create(@RequestBody Livro livro) {
+        ModelAndView mv = new ModelAndView("livros-add");
+        livrosRepository.save(livro);
+        mv.setViewName("redirect:/livros-list");
+        return mv;
     }
     
 
